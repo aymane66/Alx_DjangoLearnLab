78c4f451158ec3e219b279fc20fec054d1bf136c
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-20 23:41:57 +0100

    admin config

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
-from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Library, Book, UserProfile
-from .forms import BookForm 
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+
 
 
 def list_books(request):
