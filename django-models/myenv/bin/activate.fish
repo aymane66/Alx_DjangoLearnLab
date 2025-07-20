78127802b14fb3c7c365eccb54d91e6063d1f8d0
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-20 18:08:51 +0100

    templates

--- django-models/myenv/bin/activate.fish
+++ django-models/myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ayman/Desktop/Alx_DjangoLearnLab/Introduction_to_Django/myenv
+set -gx VIRTUAL_ENV /home/ayman/Desktop/Alx_DjangoLearnLab/django-models/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
