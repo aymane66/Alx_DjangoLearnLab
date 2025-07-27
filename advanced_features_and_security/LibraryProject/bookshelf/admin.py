692a2f95dcd86a11df4c5f51f95b675dceea03bc
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-27 04:46:17 +0100

    security enhancement add customUser

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -8,3 +11,20 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(Book, BookAdmin)
 
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'date_of_birth', 'is_staff']
+    ordering = ['email'] 
+
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    search_fields = ['email']
