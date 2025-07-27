310d8b16f5de4dfc81b9294e8b829f0a9026200c
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-27 04:44:54 +0100

    security enhancement add customUser

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
@@ -56,39 +55,5 @@ class UserProfile(models.Model):
         return f"{self.user.email} - {self.role}"
 
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-        email = self.normalize_email(email)
-        user = self.model(email=email, date_of_birth=date_of_birth, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if not extra_fields.get('is_staff'):
-            raise ValueError('Superuser must have is_staff=True.')
-        if not extra_fields.get('is_superuser'):
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, date_of_birth, password, **extra_fields)
 
 
-class CustomUser(AbstractUser):
-    username = None 
-    email = models.EmailField(_('email address'), unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['date_of_birth']
-
-    objects = CustomUserManager()
-
-    class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
