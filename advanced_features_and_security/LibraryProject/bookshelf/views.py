18c21c47b94b6d712c51ee43a76cf928b321d1a7
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-27 08:13:13 +0100

    security enhancement add customUser

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Q
-from .forms import BookForm, ExampleForm
+from .forms import ExampleForm
 
 
 from .models import Book
