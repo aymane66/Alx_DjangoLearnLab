374fef95cda079151b580af470cbc4c86bd9e4d8
Ayman <ayman.mouhcine1995@gmail.com>
2025-08-03 07:46:17 +0100

    add viewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
 
@@ -9,5 +9,23 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
+    def list(self, request):
+        books = self.queryset
+        serializer = self.serializer_class(books, many=true)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
     
