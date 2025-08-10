9e7c901ce3197990d232e9882544d640d34577bb
Ayman <ayman.mouhcine1995@gmail.com>
2025-08-10 20:40:37 +0100

    project title correction

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+
+class Author(models.Model):
+    # Represents an author with a name field
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    # Represents a book with a title, publication year, and a link to its author
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()  # Year the book was published
+
+    # ForeignKey creates a many-to-one relationship: many books can belong to one author
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,  # Deletes books if their author is deleted
+        related_name='books'       # Enables reverse lookup: author.books.all()
+    )
+
+    def __str__(self):
+        return self.title
