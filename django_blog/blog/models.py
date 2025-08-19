78c433ea0336690f9bbc6388bf76b26b6901cbea
Ayman <ayman.mouhcine1995@gmail.com>
2025-08-20 00:13:00 +0100

    blog project initiated

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    pass
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
