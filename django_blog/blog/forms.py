5448d2fd0fdfbc95f3bc2b4a83e86112a777f1b9
Ayman <ayman.mouhcine1995@gmail.com>
2025-08-23 11:01:34 +0100

    update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from django.utils.text import slugify
+from taggit.forms import TagWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -21,11 +21,6 @@ class UserUpdateForm(forms.ModelForm):
         fields = ["username", "email"]
 
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
-
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -36,54 +31,12 @@ class CommentForm(forms.ModelForm):
         }
 
 
-class PostForm(forms.ModelForm):
-    # New: a free-text field the user can type comma-separated tags into
-    tags = forms.CharField(
-        required=False,
-        help_text="Comma-separated, e.g. python, django, tips"
-    )
 
+
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # author set in the view
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Pre-fill tags when editing: "python, django"
-        if self.instance and self.instance.pk:
-            current = self.instance.tags.values_list('name', flat=True)
-            self.fields['tags'].initial = ', '.join(current)
-
-    def _parse_tags(self, text):
-        # split by comma, trim, drop empties, make unique (case-insensitive)
-        raw = [t.strip() for t in (text or '').split(',')]
-        cleaned = [t for t in raw if t]
-        # unique by lowercased name, preserve original casing of first occurrence
-        seen = {}
-        for t in cleaned:
-            key = t.lower()
-            if key not in seen:
-                seen[key] = t
-        return list(seen.values())
-
-    def save(self, commit=True):
-        # Save Post first (without M2M) to get an instance
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()
-
-        # Build Tag set from the free-text input
-        tag_names = self._parse_tags(self.cleaned_data.get('tags', ''))
-        tag_objs = []
-        for name in tag_names:
-            slug = slugify(name)
-            tag, _ = Tag.objects.get_or_create(slug=slug, defaults={'name': name})
-            # If tag existed but name casing differs, you may optionally normalize:
-            # if tag.name != name: tag.name = name; tag.save()
-            tag_objs.append(tag)
-
-        # Assign M2M
-        # (must be after instance.save(); commit=False users should handle outside)
-        if instance.pk:
-            instance.tags.set(tag_objs)
-        return instance
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
