13f0c7c85cce5dd7160dd40de9513cf1eac510b7
Ayman <ayman.mouhcine1995@gmail.com>
2025-08-27 10:47:03 +0100

    restore

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = get_user_model()
